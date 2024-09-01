c73758c30f26f555e77a3769fd52d931521bc0d8
MichaelSuarez0 <a20180264@pucp.edu.pe>
2024-08-31 22:22:17 -0500

    Update automatizacionV3.py
    Elimina líneas de figuras, tablas y notas.
    Devuelve la posición de las figuras y tablas.
    Nueva imagen en el sidebar

--- automatizacionV3.py
+++ automatizacionV3.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import re
+import random
 
 # Funciones
 # def extraer_referencias(texto_referencias):
@@ -33,6 +34,25 @@ def procesar_referencias(texto_referencias):
     return texto_limpio, referencias_internas
 
 
+def procesar_parrafos(text):
+    lines = text.split('\n')
+    patron_eliminar_figuras = re.compile(r'^(Nota?|Figura|Tabla)(\s+\d+)?.*$')
+    filtered_lines = []
+    removed_items = []
+    paragraph_index = 0
+    
+    for line in lines:
+        if patron_eliminar_figuras.match(line.strip()):
+            removed_items.append((paragraph_index, line.strip()))
+        elif line.strip():  # Si la línea no está vacía
+            paragraph_index += 1
+            filtered_lines.append(line)
+        else:
+            filtered_lines.append(line)
+
+    processed_text = '\n'.join(filtered_lines)
+    return processed_text, removed_items
+
 def crear_hipervinculo(text, referencias):
     pattern = re.compile(r'\[([\d,\s]+)\]')
     def replacement(match):
@@ -40,16 +60,37 @@ def crear_hipervinculo(text, referencias):
         links = []
         for number in numbers:
             number = int(number.strip())
-            if number in referencias and referencias[number]:  # Verifica si el número existe y tiene una URL asociada
+            if number in referencias and referencias[number]:
                 links.append(f'<a href="{referencias[number]}">[{number}]</a>')
             else:
                 links.append(f'[{number}]')
         return ' '.join(links)
     return re.sub(pattern, replacement, text)
 
+
+# Juego de imágenes
+imagenes = [
+    ("https://raw.githubusercontent.com/MichaelSuarez0/CeplanCode/main/ceplan_logo.png", 0.80),
+    ("https://raw.githubusercontent.com/MichaelSuarez0/CeplanCode/main/con_punche_peru_logo.jpg", 0.20)  # Cambia esto por la URL de tu segunda imagen
+]
+
+# Genera un número aleatorio solo una vez
+numero_aleatorio = random.random()
+
+# Suma acumulativa de probabilidades para selección correcta
+suma_probabilidad = 0
+url = None
+
+for imagen_url, probabilidad in imagenes:
+    suma_probabilidad += probabilidad
+    if numero_aleatorio < suma_probabilidad:
+        url = imagen_url
+        break
+
 # Streamlit UI
-st.sidebar.image("https://raw.githubusercontent.com/MichaelSuarez0/CeplanCode/main/ceplan_logo.png", width = 280)
-st.sidebar.header("Aquí podrás copiar")
+st.sidebar.image(url, width=280)
+st.sidebar.write(numero_aleatorio)
+st.sidebar.header("Aquí podrás copiar fácilmente")
 st.title('Aplicación para procesar texto (Observatorio)')
 
 
@@ -59,13 +100,26 @@ input_text_referencias = st.text_area('Inserta las referencias aquí', height=19
 
 boton_procesar = st.button('Procesar')
 
-st.header("Aquí podrás verificar")
+st.subheader("Aquí podrás verificar")
 
 if boton_procesar:
     referencias_limpias, referencias_internas = procesar_referencias(input_text_referencias)
-    texto_con_hipervinculos = crear_hipervinculo(input_text_main, referencias_internas)
-    st.text_area("Texto con hipervínculos:", texto_con_hipervinculos, height = 300)
-    st.text_area("Referencias limpias", referencias_limpias, height = 300)
+    texto_procesado, items_eliminados = procesar_parrafos(input_text_main)
+    texto_con_hipervinculos = crear_hipervinculo(texto_procesado, referencias_internas)
+
+    st.text_area("Texto con hipervínculos:", texto_con_hipervinculos, height = 320)
+    st.text_area("Referencias limpias", referencias_limpias, height = 290)
+
+    # Filtrar para excluir notas del orden de las figuras
+    patron_nota = re.compile(r'Nota')
+    items_eliminados_filtrados = [item for item in items_eliminados if not patron_nota.match(item[1])]
+
+    # UI (mostrar orden)
+    st.subheader("Orden de las figuras")
+    for item in items_eliminados_filtrados:
+        st.write(f"**Orden {item[0]}**: {item[1]}")
+
+    # UI (sidebar)
     st.sidebar.write("Texto")
     st.sidebar.code(texto_con_hipervinculos, language = "text")
     st.sidebar.write("Referencias")
